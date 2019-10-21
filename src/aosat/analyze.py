ad56d1f8bdb8d00c0c64360074ad392bb0cfa742
Markus Feldt <mfeldt@mpia.de>
2019-10-21 11:20:42 +0200

    analyzer.py ===========
    - added PSF analyzer
    
    frameserver.py
    ==============
    
     - fixed small unit bug

--- src/aosat/analyze.py
+++ src/aosat/analyze.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import scipy
 from astropy import units
 from astropy.io import fits as pyfits
 import pandas as pd
@@ -16,6 +17,8 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.font_manager import FontProperties
 import matplotlib.image as mpimg
+
+from astropy.modeling import models, fitting
 
 import logging
 logger = logging.getLogger(__name__)
@@ -76,7 +79,7 @@ class dmy_analyzer():
         """
         self.sd = sd
 
-    def feed_frame(self,frame):
+    def feed_frame(self,frame,nframes):
         """Accept a single frame for analysis
 
 
@@ -87,6 +90,8 @@ class dmy_analyzer():
             residual wavefront of a single timestep of the AO simulation.
             These residual WF frames are fed one by one, calculations should
             take into account this circumstance.
+        nframes: int
+            total number of rames to be fed
 
 
         Returns
@@ -99,7 +104,7 @@ class dmy_analyzer():
         >>> sd = setup()
         >>> dan = dmy_analyzer(sd)
         >>> rf  = np.zeros((1024,1024))
-        >>> dan.feed_frame(rf)
+        >>> dan.feed_frame(rf,1)
 
 
         """
@@ -126,7 +131,7 @@ class dmy_analyzer():
         >>> sd=setup()
         >>> dan = dmy_analyzer(sd)
         >>> rf = np.zeros((1024,1024))
-        >>> dan.feed_frame(rf)
+        >>> dan.feed_frame(rf,1)
         >>> dan.finalize()
         """
         pass
@@ -149,7 +154,7 @@ class dmy_analyzer():
         >>> sd=setup()
         >>> dan = dmy_analyzer(sd)
         >>> rf = np.zeros((1024,1024))
-        >>> dan.feed_frame(rf)
+        >>> dan.feed_frame(rf,1)
         >>> dan.finalize()
         >>> dan.make_report()
         'This is a dummy with no actuctual functionality'
@@ -184,7 +189,7 @@ class dmy_analyzer():
         >>> sd=setup()
         >>> dan = dmy_analyzer(sd)
         >>> rf = np.zeros((1024,1024))
-        >>> dan.feed_frame(rf)
+        >>> dan.feed_frame(rf,1)
         >>> dan.finalize()
         >>> rplot = dan.make_plot()
 
@@ -192,6 +197,176 @@ class dmy_analyzer():
         """
         fig=plt.figure()
         return(fig)
+
+
+
+##
+##
+## Simple PSF Analyzer
+##
+##
+
+
+class psf_analyzer():
+    def __init__(self,sd):
+        self.sd = sd
+        self.ffed     = 0
+        self.psf      = None
+        self.psf_mean = None
+        self.strehl   = 0.0
+        self.sr_wf    = None
+        self.ttx      = None
+        self.tty      = None
+        self.ttx_wf   = None
+        self.ttx_wf   = None
+        self.A        = None
+        self.M        = None
+        self.lmf      = fitting.LevMarLSQFitter()
+        self.x        = None
+        self.y        = None
+        xx,yy = np.indices([32,32])
+        self.sdim     = 0
+        self.xx       = xx
+        self.yy       = yy
+
+    def feed_frame(self,frame,nframes):
+        in_field  = self.sd['tel_mirror']*np.exp(1j*frame)
+        frame_dev = fftx.FFTprepare(in_field)
+        fftframe  = fftx.FFTshift(fftx.FFTforward(self.sd['fft_plan'],self.sd['fft_out'],frame_dev))
+        psf       = np.abs(fftframe)**2
+
+        if self.ffed == 0:
+            logger.debug("Initializing variables...")
+            self.psf       = psf*0
+            self.sdim      = psf.shape[0]
+            self.sr_wf     = np.zeros(nframes)
+            self.ttx       = np.zeros(nframes)
+            self.tty       = np.zeros(nframes)
+            self.ttjit     = 0.0
+            self.ttq90     = 0.0
+            self.ttjit_psf = 0.0
+            self.ttq90_psf = 0.0
+            self.tty_wf    = np.zeros(nframes)
+            self.x, self.y = np.mgrid[-self.sdim/2:self.sdim/2,-self.sdim/2:self.sdim/2]/self.sd['ppm']
+            self.A         = np.c_[self.x[self.sd['wnz']], self.y[self.sd['wnz']], self.x[self.sd['wnz']]*0.0+1.0]
+            self.M         = models.Gaussian2D(np.max(psf), 16, 16, 3, 3)
+            logger.debug("Done!")
+
+        ## Strehl from WF
+        self.sr_wf[self.ffed] = np.exp(-1*np.std(frame[self.sd['wnz']])**2)
+        logger.debug("Found SR: %s"%self.sr_wf[self.ffed])
+
+        ## mean PSF (and variance)
+        self.psf += psf
+
+        logger.debug("Solving tilt...")
+        ## tilt from WF
+        C,_,_,_ = scipy.linalg.lstsq(self.A, frame[self.sd['wnz']])#/2/np.pi*self.sd['cfg']['an_lambda']) # frame in m
+        tphase = C[0]*self.x + C[1]*self.y + C[2]
+        self.ttilt[ffed] = np.max(tphase[self.sd['wnz']]) - np.min(tphase[self.sd['wnz']])
+
+        logger.debug("Fitting position...")
+        ## tip and tilt from PSF fitted position
+        result_lmf = self.lmf(self.M, self.xx, self.yy, psf[int(self.sdim/2-16):int(self.sdim/2+16),int(self.sdim/2-16):int(self.sdim/2+16)])
+        self.ttx[self.ffed] = (result_lmf.x_mean - 16.0)*self.sd['aspp']
+        self.tty[self.ffed] = (result_lmf.y_mean - 16.0)*self.sd['aspp']
+        logger.debug("Found tt excursion of %.3f, %.3f from PSF!"%(self.ttx[self.ffed],self.tty[self.ffed]))
+
+        self.ffed += 1
+
+
+    def make_report(self):
+        report =  "##\n##\n"
+        report += "## reporting analyzer: %s\n##\n##\n" % self.__class__.__name__
+        report += "## Strehl from average's PSF peak intensity:\n"
+        report += "s_psf = %s\n##\n" % self.strehl
+        report += "## Strehl from averaged wavefront Strehls:\n"
+        report += "s_wf = %s\n##\n" % self.sr_wf.mean()
+        report += "##\n##All tt values in mas\n##\n"
+        report += "## TT jitter from average's PSF peak position:\n"
+        report += "ttjit_psf = %s\n##\n" % self.ttjit_psf
+        report += "## TT jitter from wavefront tilts:\n"
+        report += "ttjit_wf = %s\n##\n" % self.ttjit
+        report += "## TT Q90 excursion from average's PSF peak position:\n"
+        report += "ttjit_psf = %s\n##\n" % self.ttq90_psf
+        report += "## TT Q90 excursion from wavefront tilts:\n"
+        report += "ttjit_wf = %s\n##\n" % self.ttq90
+
+
+    def make_plot(self,fig=None,index=111,plotkwargs={},subplotkwargs={}):
+
+        if fig is None:
+            fig = plt.figure()
+
+        ##
+        ## default appearance
+        ##
+        if 'xlabel' not in subplotkwargs:
+            subplotkwargs['xlabel'] = '$\delta$RA[mas]'
+        if 'ylabel' not in subplotkwargs:
+            subplotkwargs['ylabel'] = '$\delta$DEC[mas]'
+        if 'title' not in subplotkwargs:
+            subplotkwargs['title'] = 'PSF'
+
+        # size of extraction area
+        plts = int(min([np.round(sd['crad']/1000.0*1.2/sd['aspp']/2.0)*2,self.sdim/2]))
+
+        if 'map' not in plotkwargs:
+            plotkwargs['map'] = 'nipy_spectral'
+        if 'norm' not in plotkwargs:
+            plotkwargs['norm'] = LogNorm(vmin=1e-7,vmax=0.5)
+        if 'extent' not in plotkwargs:
+            plotkwargs['extent'] = [plts*sd['aspp']*1000,-plts*['aspp']*1000,-plts*sd['aspp']*1000,plts*sd['aspp']*1000]
+        if 'origin' not in plotkwargs:
+            plotkwargs['origin'] = 'lower'
+        ##
+        ##  create (only) subplot
+        ##
+        logger.debug("Subplot keyword args:\n"+aosat_cfg.repString(subplotkwargs))
+        logger.debug("Plot keyword args:\n"+aosat_cfg.repString(plotkwargs))
+        ax = fig.add_subplot(index,**subplotkwargs,label=str(index*2))
+
+        im = ax.imshow(psf[self.sdim/2-plts:self.sdim/2+plts,self.sdim/2-plts:self.sdim/2+plts]/np.max(psf),
+                           **plotkwargs)
+
+        ## useful info
+        ax.text(0.7,0.9,'SR (PSF) = %.3f' % self.strehl,transform=ax.transAxes,size=6,ha='left',color='white')
+        ax.text(0.7,0.85,'SR (WF) = %.3f' % self.sr_wf,transform=ax.transAxes,size=6,ha='left',color='white')
+        ax.text(0.7,0.8,r'$\sigma_{tt}$(PSF) = %.3f mas' % self.ttjit_psf,transform=ax.transAxes,size=6,ha='left',color='white')
+        ax.text(0.7,0.75,r'$\sigma_{tt}$(WF) = %.3f mas' % self.ttjit,transform=ax.transAxes,size=6,ha='left',color='white')
+        ax.text(0.7,0.8,r'$\Q90_{tt}$(PSF) = %.3f mas' % self.ttq90_psf,transform=ax.transAxes,size=6,ha='left',color='white')
+        ax.text(0.7,0.75,r'$\Q90_{tt}$(WF) = %.3f mas' % self.ttq90,transform=ax.transAxes,size=6,ha='left',color='white')
+
+        ## color bar
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="10%", pad=0.05)
+        cax.tick_params(axis='both', which='major', labelsize=6)
+        cax.tick_params(axis='both', which='minor', labelsize=5)
+        t = [1e-7,1e-6,1e-5,1e-4,1e-3,1e-2,1e-1]
+        cbar = plt.colorbar(im, cax=cax, ticks=t,label='Intensity')
+
+
+
+
+    def finalize(self):
+        frame_dev = fftx.FFTprepare(self.sd['tel_mirror']*np.exp(1j*0.0))
+        fftframe  = fftx.FFTshift(fftx.FFTforward(self.sd['fft_plan'],self.sd['fft_out'],frame_dev))
+        psf_ref   = np.abs(tfftframe)**2
+        logger.debug("Max of reference PSF: %s" % np.max(psf_ref))
+        logger.debug("Max of measured PSF:  %s" % np.max(self.variance[1]))
+        self.psf_mean = self.psf/self.ffed
+        self.strehl   = np.max(self.psf_mean)/np.max(psf_ref)
+
+        ttmas = self.ttilt/(37.0)/np.pi*180.0*3600.0*1000.0
+
+        self.ttjit = ((ttmas**2).sum()/len(ttmas))**0.5 # tip-tilt jitter in mas
+        self.ttq90 = np.sort(ttmas)[int(len(ttmas)*0.9)]
+
+        ttmas_psf      = ((self.ttx**2+self.tty**2)**0.5)*1000.0
+        self.ttjit_psf = ((ttmas_psf**2).sum()/len(ttmas_psf))**0.5
+        self.ttq90_psf = np.sort(ttmas_psf)[int(len(ttmas_psf)*0.9)]
+
+
 
 
 class tvc_analyzer():
@@ -246,7 +421,7 @@ class tvc_analyzer():
         self.cvecmin   = None
         self.cvecmax   = None
 
-    def feed_frame(self,frame):
+    def feed_frame(self,frame,nframes):
         in_field  = self.sd['tel_mirror']*np.exp(1j*frame)
         this_strehl   = np.exp(-1*np.std(frame[self.sd['wnz']])**2) ## remember: Frames are in radians!
         logger.debug("Found Strehl ratio: %s" % this_strehl)
@@ -571,7 +746,7 @@ def run(analyzers):
             util.printProgressBar(logger,1, totnum, prefix = 'Analyzing: ', suffix = 'Complete', length = 50)
             barsteps = (np.arange(20)/20.0*totnum).astype(int)
         for analyzer in analyzers:
-            analyzer.feed_frame(frame)
+            analyzer.feed_frame(frame,totnum)
         if tfnum in barsteps:
             util.printProgressBar(logger,tfnum, totnum, prefix = 'Analyzing: ', suffix = 'Complete', length = 50)
     util.printProgressBar(logger,totnum, totnum, prefix = 'Analyzing: ', suffix = 'Complete', length = 50)
@@ -625,9 +800,9 @@ def tearsheet(config_file):
     logger.debug("\n"+aosat_cfg.repString(sd))
 
     ##
-    ## add all available analyzers, tzhen run
+    ## add all available analyzers, then run
     ##
-    analyzers=[tvc_analyzer(sd,ctype='icor'), tvc_analyzer(sd)]
+    analyzers=[psf_analyzer(sd), tvc_analyzer(sd,ctype='icor'), tvc_analyzer(sd)]
     run(analyzers)
 
     ##
