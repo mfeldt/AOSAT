4919e566d44b05eaf9877c5bdd28796610d695c6
BuildTools <unconfigured@null.spigotmc.org>
2020-05-18 16:04:37 +0200

    CUPYized tvc_analyzer

--- src/aosat/_analyzers/tvc_analyzer.py
+++ src/aosat/_analyzers/tvc_analyzer.py
@@ -1,7 +1,11 @@
 
 
 import os
-import numpy as np
+from pip._internal.utils.misc import get_installed_distributions
+if any(["cupy" in str(f) for f in get_installed_distributions()]):
+    import cupy as np
+else:
+    import numpy as np
 import scipy
 from astropy import units
 from astropy.io import fits as pyfits
@@ -146,25 +150,25 @@ class tvc_analyzer():
         ##
         logger.debug("Subplot keyword args:\n"+aosat_cfg.repString(subplotkwargs))
         ax = fig.add_subplot(index,**subplotkwargs,label=str(index*2))
-        ax.fill_between(self.rvec,self.cvecmin,self.cvecmax,alpha=0.15,**plotkwargs)
-        ax.plot(self.rvec,self.cvecmean,**plotkwargs)
+        ax.fill_between(util.ensure_numpy(self.rvec),util.ensure_numpy(self.cvecmin),util.ensure_numpy(self.cvecmax),alpha=0.15,**plotkwargs)
+        ax.plot(util.ensure_numpy(self.rvec),util.ensure_numpy(self.cvecmean),**plotkwargs)
         if self.corrlen > 0:
             num_frames_per_hour = 3600.0/(self.corrlen/self.sd['loopfreq'])
             plotkwargsP = copy.copy(plotkwargs)
             plotkwargsP['linestyle'] = 'dotted'
-            ax.plot(self.rvec,self.cvecmean/(num_frames_per_hour**0.5),**plotkwargsP)
+            ax.plot(util.ensure_numpy(self.rvec),util.ensure_numpy(self.cvecmean)/(num_frames_per_hour**0.5),**plotkwargsP)
 
         ax.text(0.5,0.9,'No photon noise,',transform=ax.transAxes,size=6,ha='left',color=plotkwargs['color'])
         ax.text(0.5,0.85,'due to PSF variation only!',transform=ax.transAxes,size=6,ha='left',color=plotkwargs['color'])
         ax.text(0.5,0.95,r'5 $\sigma$ TVC',transform=ax.transAxes,size=6,ha='left',color=plotkwargs['color'])
         plotkwargs2 = copy.copy(plotkwargs)
         plotkwargs2['color']='black'
-        ax.fill_between(self.rvec,self.rcvecmin,self.rcvecmax,alpha=0.15,**plotkwargs2)
-        ax.plot(self.rvec,self.rcvecmean,**plotkwargs2)
+        ax.fill_between(util.ensure_numpy(self.rvec),util.ensure_numpy(self.rcvecmin),util.ensure_numpy(self.rcvecmax),alpha=0.15,**plotkwargs2)
+        ax.plot(util.ensure_numpy(self.rvec),util.ensure_numpy(self.rcvecmean),**plotkwargs2)
         if self.corrlen > 0:
             plotkwargsP = copy.copy(plotkwargs2)
             plotkwargsP['linestyle'] = 'dotted'
-            ax.plot(self.rvec,self.rcvecmean/(num_frames_per_hour**0.5),**plotkwargsP)
+            ax.plot(util.ensure_numpy(self.rvec),util.ensure_numpy(self.rcvecmean)/(num_frames_per_hour**0.5),**plotkwargsP)
         ax.text(0.5,0.8,'Raw (PSF profile)',transform=ax.transAxes,size=6,ha='left',color=plotkwargs2['color'])
         if self.corrlen>0:
             ax.text(0.5,0.75,'Dotted lines: est. best 1hr ADI contrast',transform=ax.transAxes,size=6,ha='left',color=plotkwargs2['color'])
@@ -174,28 +178,30 @@ class tvc_analyzer():
 
     def make_report(self):
 
-        rep_vec_r     = np.arange(50)*self.sd['aspp']
+        rep_vec_r     = util.ensure_numpy(np.arange(50)*self.sd['aspp'])
 
-        rep_vec_cmean = np.interp(rep_vec_r,self.rvec,self.cvecmean)
-        rep_vec_cmin  = np.interp(rep_vec_r,self.rvec,self.cvecmin)
-        rep_vec_cmax  = np.interp(rep_vec_r,self.rvec,self.cvecmax)
+        from numpy import interp, array2string
 
-        rep_vec_rcmean = np.interp(rep_vec_r,self.rvec,self.rcvecmean)
-        rep_vec_rcmin  = np.interp(rep_vec_r,self.rvec,self.rcvecmin)
-        rep_vec_rcmax  = np.interp(rep_vec_r,self.rvec,self.rcvecmax)
+        rep_vec_cmean = interp(rep_vec_r,util.ensure_numpy(self.rvec),util.ensure_numpy(self.cvecmean))
+        rep_vec_cmin  = interp(rep_vec_r,util.ensure_numpy(self.rvec),util.ensure_numpy(self.cvecmin))
+        rep_vec_cmax  = interp(rep_vec_r,util.ensure_numpy(self.rvec),util.ensure_numpy(self.cvecmax))
+
+        rep_vec_rcmean = interp(rep_vec_r,util.ensure_numpy(self.rvec),util.ensure_numpy(self.rcvecmean))
+        rep_vec_rcmin  = interp(rep_vec_r,util.ensure_numpy(self.rvec),util.ensure_numpy(self.rcvecmin))
+        rep_vec_rcmax  = interp(rep_vec_r,util.ensure_numpy(self.rvec),util.ensure_numpy(self.rcvecmax))
 
         report =  "##\n##\n"
         report += "## reporting analyzer: %s\n##\n##\n" % self.__class__.__name__
         report += "## contrast type: %s\n" % self.ctype
         report += "## Interpolated contrast values:\n\n"
-        report += "tvc_%s_sepvec    = %s\n" % (self.ctype,np.array2string(rep_vec_r,separator=',',precision=4))
-        report += "tvc_%s_ctrstmean = %s\n" % (self.ctype,np.array2string(rep_vec_cmean,separator=','))
-        report += "tvc_%s_ctrstmin  = %s\n" % (self.ctype,np.array2string(rep_vec_cmin,separator=','))
-        report += "tvc_%s_ctrstmax  = %s\n\n\n" % (self.ctype,np.array2string(rep_vec_cmax,separator=','))
-        report += "raw_%s_sepvec    = %s\n" % (self.ctype,np.array2string(rep_vec_r,separator=',',precision=4))
-        report += "raw_%s_ctrstmean = %s\n" % (self.ctype,np.array2string(rep_vec_rcmean,separator=','))
-        report += "raw_%s_ctrstmin  = %s\n" % (self.ctype,np.array2string(rep_vec_rcmin,separator=','))
-        report += "raw_%s_ctrstmax  = %s\n##\n" % (self.ctype,np.array2string(rep_vec_rcmax,separator=','))
+        report += "tvc_%s_sepvec    = %s\n" % (self.ctype,array2string(rep_vec_r,separator=',',precision=4))
+        report += "tvc_%s_ctrstmean = %s\n" % (self.ctype,array2string(rep_vec_cmean,separator=','))
+        report += "tvc_%s_ctrstmin  = %s\n" % (self.ctype,array2string(rep_vec_cmin,separator=','))
+        report += "tvc_%s_ctrstmax  = %s\n\n\n" % (self.ctype,array2string(rep_vec_cmax,separator=','))
+        report += "raw_%s_sepvec    = %s\n" % (self.ctype,array2string(rep_vec_r,separator=',',precision=4))
+        report += "raw_%s_ctrstmean = %s\n" % (self.ctype,array2string(rep_vec_rcmean,separator=','))
+        report += "raw_%s_ctrstmin  = %s\n" % (self.ctype,array2string(rep_vec_rcmin,separator=','))
+        report += "raw_%s_ctrstmax  = %s\n##\n" % (self.ctype,array2string(rep_vec_rcmax,separator=','))
         report += "## estimated correlation time [ms] \n##\n"
         report += "estim_corr_time   = %s \n\n\n" % (1000*self.corrlen/self.sd['loopfreq'])
         return(report)
@@ -214,8 +220,8 @@ class tvc_analyzer():
         ra,pixels,rord = util.rad_order(self.contrast)
 
         self.rvec  = ra[pixels][rord]*self.sd['aspp']
-        c          = pd.Series(self.contrast[pixels][rord])
-        r          = pd.Series(self.rcontrast[pixels][rord])
+        c          = pd.Series(util.ensure_numpy(self.contrast[pixels][rord]))
+        r          = pd.Series(util.ensure_numpy(self.rcontrast[pixels][rord]))
 
         self.cvecmean = c.rolling(window=50).mean()
         self.cvecmin  = c.rolling(window=50).min()
@@ -226,8 +232,8 @@ class tvc_analyzer():
 
         max_corr_len=0.0
         for i in range(self.ntracks):
-            acff,conf    = acf(self.ptrak[:,i],fft=True,alpha=0.05,nlags=min([500,len(self.ptrak[:,i])-1]))
-            where_uncorr = np.where(acff < (conf[:,1] - acff))[0]
+            acff,conf    = acf(util.ensure_numpy(self.ptrak[:,i]),fft=True,alpha=0.05,nlags=min([500,len(self.ptrak[:,i])-1]))
+            where_uncorr = np.where(np.array(acff) < (np.array(conf[:,1]) - np.array(acff)))[0]
             if len(where_uncorr) > 0:
-                max_corr_len = np.max([max_corr_len,np.min(where_uncorr)])
-        self.corrlen = max_corr_len*1.0
+                max_corr_len = max([max_corr_len,np.min(where_uncorr)])
+        self.corrlen = max_corr_len.item()*1.0
