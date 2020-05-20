881ffe973fd98367e316bdf84ae2084a6c259bd2
BuildTools <unconfigured@null.spigotmc.org>
2020-05-20 11:39:05 +0200

    CUDAization completed

--- src/aosat/_analyzers/dmy_analyzer.py
+++ src/aosat/_analyzers/dmy_analyzer.py
@@ -1,5 +1,10 @@
 import os
-import numpy as np
+
+from pip._internal.utils.misc import get_installed_distributions
+if any(["cupy" in str(f) for f in get_installed_distributions()]):
+    import cupy as np
+else:
+    import numpy as np
 import scipy
 from astropy import units
 from astropy.io import fits as pyfits
